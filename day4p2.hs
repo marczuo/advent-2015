d9915a0ddce32ae667442f6deb2a1afc2ba79d74
Marc Zuo <zealotzuo@gmail.com>
2015-12-04 16:10:58 +0100

    Slight change for readability

--- day4p2.hs
+++ day4p2.hs
@@ -13,7 +13,7 @@ md5StartsWithZeros :: String -> Bool
 md5StartsWithZeros = isPrefixOf "000000" . md5AsString
 
 findAnswer :: String -> Int
-findAnswer prefix = length $ takeWhile (not . md5StartsWithZeros) (map (((++) prefix) . show) [0..])
+findAnswer prefix = length $ takeWhile (not . md5StartsWithZeros) (map ((prefix++) . show) [0..])
 
 main :: IO ()
 main = readApplyPrint argsToFileName parseContent findAnswer where
