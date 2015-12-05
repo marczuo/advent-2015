d4e1f53819da6891b473065464c388d90dd67177
Marc Zuo <zealotzuo@gmail.com>
2015-12-05 14:57:32 +0100

    rv debug code

--- day5p2.hs
+++ day5p2.hs
@@ -5,24 +5,17 @@ import Data.List
 import IO.ReadApplyPrint
 
 isNice :: String -> Bool
-isNice = (&&) <$> hasPair <*> hasRepeat
+isNice = (&&) <$> hasPair <*> hasRepeat where 
 
-hasPair :: Eq a => [a] -> Bool
-hasPair [] = False; hasPair [x] = False; hasPair [x,y] = False
-hasPair (x:xs) = isInfixOf [x, head xs] (tail xs) || hasPair xs
+    hasPair :: Eq a => [a] -> Bool
+    hasPair [] = False; hasPair [x] = False; hasPair [x,y] = False
+    hasPair (x:xs) = isInfixOf [x, head xs] (tail xs) || hasPair xs
 
-hasRepeat :: Eq a => [a] -> Bool
-hasRepeat str = foldr (||) False $ map (uncurry (==)) $ (zip <*> (tail . tail)) str
+    hasRepeat :: Eq a => [a] -> Bool
+    hasRepeat str = foldr (||) False $ map (uncurry (==)) $ (zip <*> (tail . tail)) str
 
 countNice :: [String] -> Int
 countNice = length . filter isNice
-
-toVerboseOutput :: [String] -> String
-toVerboseOutput = unlines . (map processString) where
-    processString str = str ++ " is " ++ (if isNice str then "nice" else "naughty")
-                            ++ " : it " ++ (if hasPair str then "has" else "does not have")
-                            ++ " a repeated pair, and " ++ (if hasRepeat str then "has" else "does not have")
-                            ++ " a split run of two."
 
 main :: IO ()
 main = readApplyPrint argsToFileName parseContent findAnswer where
