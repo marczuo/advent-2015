7b63f27b114f3bffb37dd079adfda1b57aaa31b8
Marc Zuo <zealotzuo@gmail.com>
2015-12-13 03:35:16 +0100

    Function stylistic changes

--- Local/Data/List.hs
+++ Local/Data/List.hs
@@ -3,20 +3,14 @@ module Local.Data.List ( combination, combinationNoDiag, slice ) where
 import Control.Monad
 import Data.List
 
--- The following pointfree-fu uses the fact that
---     \x -> f x (g x)
--- is equivalent to
---     g >>= flip f
--- in monadic notation.
-
--- The function itself is modified from this StackExchange answer:
+-- The function is from this StackExchange answer:
 --     http://stackoverflow.com/a/25900462
 
 combination :: [a] -> [(a,a)]
-combination = join . (tails >>= flip (zipWith (zip . repeat)))
+combination = join . (zipWith (zip . repeat) `ap` tails)
 
 combinationNoDiag :: [a] -> [(a,a)]
-combinationNoDiag = join . (tail . tails >>= flip (zipWith (zip . repeat)))
+combinationNoDiag = join . (zipWith (zip . repeat) `ap` (tail . tails))
 
 slice :: [a] -> Int -> Int -> [a]
 slice list i n = take n $ drop i list
